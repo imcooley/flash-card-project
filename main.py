6de7ba5b7ef9aecd7165f58fdee0191fa94ee31f
imcooley <39498371+imcooley@users.noreply.github.com>
2021-07-28 18:00:49 -0400

    Added tkinker

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from tkinter import *
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
-
+words = pandas.read_csv("./data/french_words.csv")
+records = words.DataFrame.to_dict(orient="records")
 
 window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50)
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1)
+# ---------------------------- Canvas ---------------------------- #
+canvas = Canvas(height=526, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="./images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.grid(row=0, column=0, columnspan=2)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 
+# ---------------------------- Buttons ---------------------------- #
+check_image = PhotoImage(file="./images/right.png")
+unknown_button = Button(image=check_image, highlightthickness=0)
+unknown_button.grid(row=1, column=1)
+cross_image = PhotoImage(file="./images/wrong.png")
+wrong_button = Button(image=cross_image, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
 
-
-my_image = PhotoImage(file="path/to/image_file.png")
-button = Button(image=my_image, highlightthickness=0)
 
 window.mainloop()
 
